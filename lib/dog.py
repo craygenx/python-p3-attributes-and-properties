dcfde1b862ddc7464776df8d2159cfa701afadb6
craygenx <marshmellowking99@gmail.com>
2023-08-24 13:07:25 +0300

    Completed asignment

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,28 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self._name = name
+        self._breed = breed
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value
+    
+    @property
+    def breed(self):
+        return self._breed
+    
+    @breed.setter
+    def breed(self, value):
+        if value not in Dog.approved_breeds:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = value
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,28 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name):
+        self._name = name.title()
+        self._job = None
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or len(value) >= 25:
+            print("Name must be string under 25 characters.")
+        else:
+            self._name = value.title()
+    
+    @property
+    def job(self):
+        return self._job
+    
+    @job.setter
+    def job(self, value):
+        if value not in Person.approved_jobs:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = value
